d9508114b18517ef367ef88ad11aebdadce57934
BIT <BIT@DESKTOP-998OGBN>
2021-04-27 12:30:55 +0900

    2

--- practice02/src/main/java/prob03/Prob03.java
+++ practice02/src/main/java/prob03/Prob03.java
@@ -1,12 +1,23 @@
 package prob03;
 
+import java.util.Scanner;
+
 public class Prob03 {
+	public void printCharArray(char[] c) {
+		
+	}
+	
 	public static void main(String args[]) {
 		char c[] = { 'T', 'h', 'i', 's', ' ', 'i', 's', ' ', 'a', ' ', 'p', 'e', 'n', 'c', 'i', 'l', '.' };
 
+				
+	
 		// 원래 배열 원소 출력
 		printCharArray(c);
 
+		Scanner scanner = new Scanner(System.in);
+		
+		
 		// 공백 문자 바꾸기
 		replaceSpace(c);
 
