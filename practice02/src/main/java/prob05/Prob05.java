4ee695dad1ca2a6e633ec609f301ead925c2f535
BIT <BIT@DESKTOP-998OGBN>
2021-04-27 17:54:22 +0900

    마지막문제

--- practice02/src/main/java/prob05/Prob05.java
+++ practice02/src/main/java/prob05/Prob05.java
@@ -6,26 +6,39 @@ import java.util.Scanner;
 public class Prob05 {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner( System.in );
-
-		while( true ) {
-			
-			/* 게임 작성 */
-
-			// 정답 램덤하게 만들기
-			Random random = new Random();
-			int correctNumber = random.nextInt( 100 ) + 1;
-			System.out.println(correctNumber);
-
-			
-			//새 게임 여부 확인하기
-			System.out.print( "다시 하겠습니까(y/n)>>" );
-			String answer = scanner.next();
-			if( "y".equals( answer ) == false ) {
-				break;
-			}
-		}
+		int temp = 0;
+		int value = 0;
+		Scanner scanner = new Scanner(System.in);
+		// 정답 램덤하게 만들기
+		Random random = new Random();
+		int correctNumber = random.nextInt(100) + 1;
+		System.out.println(correctNumber);
 		
+		while (true) {
+
+		
+			/* 게임 작성 */
+			value = scanner.nextInt();
+			if (correctNumber == value) {
+				System.out.println("정답!" + value);
+				// 새 게임 여부 확인하기
+				System.out.print("다시 하겠습니까(y/n)>>");
+				String answer = scanner.next();
+
+				if ("y".equals(answer) == false) {
+					break;
+				}
+			} else {
+
+				if (value < correctNumber) {
+					System.out.println("땡! " + " " + value + "~" + "100 ");
+				} else {
+					System.out.println("땡! " + " " + correctNumber + "~" + value);
+				}
+			}
+
+		}
+
 		scanner.close();
 	}
 
