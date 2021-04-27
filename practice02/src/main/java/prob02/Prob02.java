e9be3005627954662c045c04c3ba2ff9f0d3bd79
BIT <BIT@DESKTOP-998OGBN>
2021-04-27 12:48:55 +0900

    test

--- practice02/src/main/java/prob02/Prob02.java
+++ practice02/src/main/java/prob02/Prob02.java
@@ -1,5 +1,7 @@
 package prob02;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Prob02 {
@@ -7,15 +9,30 @@ public class Prob02 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
-		int[] intArray = new int[ 5 ];
-		double sum = 0;
 
+		double sum = 0;
+		
 		/* 키보드에서 배열 크기만큼 입력 받아 배열에 저장하는 코드 */
+		System.out.println("배열 크기 입력");
+		int value = scanner.nextInt();
+		
+		int[] intArray = new int[ value ];
+	    
+		for(int i = 0; i < intArray.length; i++) {
+			System.out.println("값 입력 index : " + i );
+			value = scanner.nextInt();
+		    intArray[i] = i;
+		}
+		
 		
 		/* 배열에 저장된 정수 값 더하기 */
+		for(int i = 0 ; i < intArray.length; i++) {
+			sum = sum + intArray[i];
+		}
 		
 		/* 출력 */
 		
+		System.out.println((int)sum);
 		/* 자원정리 */
 		scanner.close();
 	}
--- practice02/src/main/java/prob03/Prob03.java
+++ practice02/src/main/java/prob03/Prob03.java
@@ -3,21 +3,26 @@ package prob03;
 import java.util.Scanner;
 
 public class Prob03 {
-	public void printCharArray(char[] c) {
-		
+	public static void printCharArray(char[] c) {		
+		for(char value : c) {			
+			System.out.print(value + "");			
+		}
+
 	}
-	
+
+	public static void replaceSpace(char[] c) {
+
+	}
+
+
 	public static void main(String args[]) {
 		char c[] = { 'T', 'h', 'i', 's', ' ', 'i', 's', ' ', 'a', ' ', 'p', 'e', 'n', 'c', 'i', 'l', '.' };
 
-				
-	
 		// 원래 배열 원소 출력
 		printCharArray(c);
 
 		Scanner scanner = new Scanner(System.in);
-		
-		
+
 		// 공백 문자 바꾸기
 		replaceSpace(c);
 
