883687af508c30f445d7155e015d1bc89e7a5c75
BIT <BIT@DESKTOP-998OGBN>
2021-04-27 13:01:21 +0900

    dd

--- practice02/src/main/java/prob04/Prob04.java
+++ practice02/src/main/java/prob04/Prob04.java
@@ -15,8 +15,8 @@ public class Prob04 {
 		
 		for(int i = 0 ; i < 10; i++)
 		{
-			System.out.print(str.charAt(i));
-		
+			System.out.println(str.charAt(i));
+			reverse[i] = str.charAt(i);
 			
 		}
 		/* 코드를 완성합니다 */
